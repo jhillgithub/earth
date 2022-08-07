8dc571d1ee3c2cddd15c9d49ed62b4d1cb6cd0a1
jhill <jeremy.hill.email@gmail.com>
2022-08-06 19:56:52 -0500

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Environment, OrbitControls, Stars, Lightformer, ContactShadows } from "@react-three/drei";
+import { EffectComposer, DepthOfField, Bloom, Noise, Vignette } from '@react-three/postprocessing';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import { Globe } from "./globe";
+
+const App = () => {
+    return (
+        <Canvas shadows camera={{ position: [0, 1, 8], fov: 30 }}>
+            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+            {/* <spotLight position={[-5, 0, 1]} angle={0.3} penumbra={.5} castShadow intensity={2} shadow-bias={-0.0001} /> */}
+            <pointLight position={[10, 10, 10]} />
+            <hemisphereLight args={[0xffffbb, 0x080820, 1]} />
+            <directionalLight position={[-10, 0, -5]} intensity={.55} color="red" />
+            <directionalLight position={[-1, -2, -5]} intensity={0.2} color="#0c8cbf" />
+            <spotLight position={[5, 0, 5]} intensity={2.5} penumbra={1} angle={0.35} castShadow color="#0c8cbf" />
+            <ambientLight intensity={0.2} />
+            <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={10} blur={3} opacity={1} far={10} />
+
+
+            <color attach="background" args={['#15151a']} />
+            <Suspense>
+                <Environment preset="city" />
+                {/* <fog attach="fog" s args={["blue", 3, 20]} /> */}
+                <OrbitControls />
+                <Globe />
+                <EffectComposer>
+                    {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
+                    <Bloom luminanceThreshold={0} luminanceSmoothing={0.5} height={300} />
+                    <Noise opacity={0.02} />
+                    <Vignette eskil={false} offset={0.1} darkness={1.1} />
+                </EffectComposer>
+            </Suspense>
+        </Canvas>
+    );
+};
+
+export default App;
---
+++ src/globe.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useMemo, useRef } from "react";
+import { Sparkles, useTexture } from "@react-three/drei";
+import { useFrame } from '@react-three/fiber';
+import map from '../assets/earth_diffuseOriginal.jpg';
+import normalMap from '../assets/earth_normal.jpg';
+import displacementMap from '../assets/earth_height.jpg';
+import aoMap from '../assets/earth_ao.jpg';
+export const Globe = ({ displacementScale = 0.025, size = 1, amount = 50, color = 'white', emissive, glow, ...props }) => {
+  const ref = useRef();
+  const maps = useTexture({
+    map,
+    normalMap,
+    displacementMap,
+    aoMap,
+  });
+
+  useFrame((state, delta) => (ref.current.rotation.y += 0.0005));
+  return (
+    <group>
+
+      <Sparkles count={amount} scale={size * 2} size={2} speed={.1} />
+      <mesh ref={ref}>
+        <sphereGeometry args={[1, 100, 100]} />
+        <meshStandardMaterial
+          {...maps}
+        // roughness={1}
+        />
+      </mesh>
+    </group>
+  );
+};
